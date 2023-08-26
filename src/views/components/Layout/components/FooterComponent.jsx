3383b9e9bc7b595296bada93b60f0dd19687b10f
yammaru <63098405+yammaru@users.noreply.github.com>
2023-08-26 03:57:58 -0500

    feat:nuevo footer

--- src/views/components/Layout/components/FooterComponent.jsx
+++ src/views/components/Layout/components/FooterComponent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Divider, Layout } from "antd";
+import { Divider, Layout, Row } from "antd";
 import Contacte from "./FooterItems/Contacte";
 import SocialMediaFooter from "./FooterItems/SocialMediaFooter";
 import DownFooter from "./FooterItems/DownFooter";
@@ -11,26 +11,65 @@ const FooterComponent = () => {
 	let today = new Date(Date.now());
 
 	return (
-		<div
-			className="footer-anga"
-			style={{
-				textAlign: "center",
-				backgroundColor: "white",
-				width: "100%",
-				height: "auto",
-			
-			
-				
-				//zIndex: 99,
-			}}
-		>
-			<Divider style={{ width: "100%" , visibility:"hidden"}} />
-			<UpFooter />
+		<>
+			<div
+				className="footer-anga"
+				style={{
+					textAlign: "center",
+					backgroundColor: "white",
+					width: "100%",
+					height: "auto",
 
-			<Divider style={{ width: "100%" }} />
-			<DownFooter />
-			<Divider style={{ width: "100%" , visibility:"hidden"}} />
-		</div>
+					//zIndex: 99,
+				}}
+			>
+				{window.location.pathname.includes("checkout") ? (
+					<><Row justify={"center"} 	style={{
+								backgroundColor: "#f2f2f2",
+								color: "1e1e1e",
+							}}>
+							
+							<Divider
+								style={{ width: "100%", visibility: "hidden" }}
+							/>
+							Medios de pago y sitio seguro
+							<Divider
+								style={{ width: "100%", visibility: "hidden" }}
+							/>
+						
+					</Row><Row style={{
+								backgroundColor: "#484848",
+								color: "white",
+								height :"40px",
+								display:"flex",
+								justifyContent:"center",
+								alignItems:"center"
+							}}>
+					
+							Todos los derechos reservados. Copyright ©{today.getFullYear().toString()} Anga Col ·
+						
+						</Row>
+					
+					
+
+						
+					</>
+				) : (
+					<>
+						<Divider
+							style={{ width: "100%", visibility: "hidden" }}
+						/>
+						<UpFooter />
+
+						<Divider style={{ width: "100%" }} />
+						<DownFooter />
+						<Divider
+							style={{ width: "100%", visibility: "hidden" }}
+						/>
+					</>
+				)}
+			</div>
+		</>
 	);
 };
 export default FooterComponent;
