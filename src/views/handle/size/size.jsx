7ef78606e734c97e85ac041539128ab3f4612cb3
yammaru <63098405+yammaru@users.noreply.github.com>
2023-09-15 09:26:27 -0500

    feat: creacuon de funcionalidad  de verificacon de  ancho de pagina

---
+++ src/views/handle/size/size.jsx
@@ -0,0 +1,17 @@
+
+export function useWindowWidth(useState, useEffect) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowWidth;
+}
