7b254d1d6571892d94e3b21e869f504763e7dbc5
yammaru <63098405+yammaru@users.noreply.github.com>
2023-09-15 09:24:53 -0500

    divicion de la parte isquierda del menu

---
+++ src/views/components/Layout/components/NavMenuItems/LeftItems.jsx
@@ -0,0 +1,55 @@
+import logo from "../../../../../includes/images/logoInicio.png";
+import logoHover from "../../../../../includes/images/logoIniciohover.png";
+import logoCheck from "../../../../../includes/images/logocheout.png";
+import logoKids from "../../../../../includes/images/angaKids.png";
+import logoKidsHover from "../../../../../includes/images/angaKidsColor.png";
+import { Image } from "antd";
+import { useState } from "react";
+
+const LeftItems = () => {
+	const handleWhenLogo = () => {
+		if (window.location.pathname.includes("kids")) {
+			return logoKids;
+		} else if (window.location.pathname.includes("checkout")) {
+			return logoCheck;
+		} else {
+			return logo;
+		}
+	};
+	const [logoMenu, setLogoMenu] = useState(handleWhenLogo);
+
+	const handleChange = () => {
+		if (window.location.pathname.includes("kids")) {
+			setLogoMenu(logoKidsHover);
+		} else {
+			setLogoMenu(logoHover);
+		}
+	};
+	const handleChangeLeave = () => {
+		if (window.location.pathname.includes("kids")) {
+			setLogoMenu(logoKids);
+		} else {
+			setLogoMenu(logo);
+		}
+	};
+	return (
+		<a href="/">
+			<Image
+				className="custom-image"
+				style={{
+					height: "28px",
+					width: "auto",
+				}}
+				src={
+					window.location.pathname.includes("checkout")
+						? logoCheck
+						: logoMenu
+				}
+				preview={false}
+				onMouseOver={handleChange}
+				onMouseLeave={handleChangeLeave}
+			/>
+		</a>
+	);
+};
+export default LeftItems;
