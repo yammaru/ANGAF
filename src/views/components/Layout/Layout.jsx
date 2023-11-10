cee26d80f2033ca0ddd73f8a0e08768eff45f5bc
yammaru <63098405+yammaru@users.noreply.github.com>
2023-11-10 11:32:45 -0500

    agregando la a en las opciones de footer

--- src/views/components/Layout/Layout.jsx
+++ src/views/components/Layout/Layout.jsx
@@ -9,12 +9,14 @@ import { logoutUser } from "../../../redux/actions/Configuration/authAction";
 import logo from "../../../includes/images/nubeHover.png";
 import logoHover from "../../../includes/images/nube.png";
 import chat from "../../../includes/images/CHATaNGA.png";
-import { useWindowWidth  } from "../../handle/size/size";
+import ASimple from "../../../includes/images/nav.png";
+import { useWindowWidth } from "../../handle/size/size";
 import { useEffect } from "react";
+import LeftItems from "./components/NavMenuItems/LeftItems";
+import { Hidden } from "@material-ui/core";
 const { Content } = Layout;
 
 const LayoutComponent = ({ user, dispatchLogoutAction }) => {
-	
 	const anchoPagina = useWindowWidth(useState, useEffect);
 	const [logoMenu, setLogoMenu] = useState(logo);
 	const handleChange = () => {
@@ -26,18 +28,26 @@ const LayoutComponent = ({ user, dispatchLogoutAction }) => {
 
 	return (
 		<Fragment>
-			<NavMenu anchoPagina={anchoPagina}/>
-			{
-				 window.location.pathname !== "/"?<Divider style={{ paddingBottom: anchoPagina<=766?"10%":"2%" }} />:<></>
-			}
-			
+			<NavMenu anchoPagina={anchoPagina} />
+			{window.location.pathname !== "/" ? (
+				<Divider
+					style={{ paddingBottom: anchoPagina <= 766 ? "10%" : "2%" }}
+				/>
+			) : (
+				<></>
+			)}
 			{/* RUTAS */}
 			<Routes />
 			{/* END RUTAS */}
-
-		
 			{window.location.pathname.includes("checkout") ? null : (
-				<div style={{ position: "fixed", right: "2%", bottom: "6%",zIndex:5000 }}>
+				<div
+					style={{
+						position: "fixed",
+						right: "2%",
+						bottom: "6%",
+						zIndex: 5000,
+					}}
+				>
 					<a
 						href="https://api.whatsapp.com/send/?phone=573013095065"
 						target="_blank"
@@ -53,7 +63,13 @@ const LayoutComponent = ({ user, dispatchLogoutAction }) => {
 							/>
 						) : (
 							<Image
-								
+								style={{
+									display: window.location.pathname.includes(
+										"yourlook"
+									)
+										? "none"
+										: "",
+								}}
 								height={50}
 								src={chat}
 								preview={false}
@@ -61,8 +77,28 @@ const LayoutComponent = ({ user, dispatchLogoutAction }) => {
 						)}
 					</a>
 				</div>
-			)}	
-			<FooterComponent  anchoPagina={anchoPagina}/>
+			)}
+			{anchoPagina<=574?
+				<Row justify={"center"} style={{ width: "100%" }}>
+					<Divider
+						style={{
+							visibility: "hidden",
+						}}
+					/>
+					<a href={"/"}>
+						<Image
+							className="custom-image"
+							style={{
+								height: "28px",
+								width: "auto",
+							}}
+							src={ASimple}
+							preview={false}
+						/>
+					</a>
+				</Row>:<></>
+			}
+			<FooterComponent anchoPagina={anchoPagina} />
 		</Fragment>
 	);
 };
