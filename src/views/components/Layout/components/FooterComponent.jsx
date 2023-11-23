87a7dfab71f5eaa33c78f45a5a4641e6f8b28f50
yammaru <63098405+yammaru@users.noreply.github.com>
2023-11-23 13:15:42 -0500

    agregar logo de yammco dev

--- src/views/components/Layout/components/FooterComponent.jsx
+++ src/views/components/Layout/components/FooterComponent.jsx
@@ -1,73 +1,84 @@
 import React from "react";
-import { Divider, Layout, Row } from "antd";
+import { Divider, Layout, Row, Col, Image } from "antd";
 import Contacte from "./FooterItems/Contacte";
 import SocialMediaFooter from "./FooterItems/SocialMediaFooter";
 import DownFooter from "./FooterItems/DownFooter";
 import UpFooter from "./FooterItems/UpFooter";
-
+import pingu from "../../../../includes/images/eL PIGUINO SABIO.png";
 const { Footer } = Layout;
 
-const FooterComponent = ({anchoPagina}) => {
+const FooterComponent = ({ anchoPagina }) => {
 	let today = new Date(Date.now());
 
-	return (	
-			<Row
-				//className="footer-anga"
-				style={{
-					textAlign: "center",
-					backgroundColor: "white",
-					width: "100%",
-					height: "auto",
+	return (
+		<Row
+			//className="footer-anga"
+			style={{
+				textAlign: "center",
+				backgroundColor: "white",
+				width: "100%",
+				height: "auto",
 
-					//zIndex: 99,
-				}}
-			>
-				{window.location.pathname.includes("checkout") ? (
-					<><Row justify={"center"} 	style={{
-								backgroundColor: "#f2f2f2",
-								color: "1e1e1e",
-							}}>
+				//zIndex: 99,
+			}}
+		>
+			{window.location.pathname.includes("checkout") ? (
+				<Row
+					justify={"center"}
+					style={{
+						backgroundColor: "#f2f2f2",
+						color: "1e1e1e",
+						width: "100%",
+					}}
+				>
+					<Row
+						justify={"space-around"}
+						style={{
+							width: "100%",
+							height: 80,
+							display: "flex",
 							
-							<Divider
-								style={{ width: "100%", visibility: "hidden" }}
-							/>
-							Medios de pago y sitio seguro
-							<Divider
-								style={{ width: "100%", visibility: "hidden" }}
-							/>
-						
-					</Row><Row style={{
-								backgroundColor: "#484848",
-								color: "white",
-								height :"40px",
-								display:"flex",
-								justifyContent:"center",
-								alignItems:"center"
-							}}>
-					
-							Todos los derechos reservados. Copyright ©{today.getFullYear().toString()} Anga Col ·
-						
-						</Row>
-					
-					
+							alignItems: "center",
+						}}
+					>
+						Medios de pago y sitio seguro
+					</Row>
 
-						
-					</>
-				) : (
-					<>
-						<Divider
-							style={{ width: "100%", visibility: "hidden" }}
-						/>
-						<UpFooter  anchoPagina={anchoPagina}/>
+					<Row
+						style={{
+							width: "100%",
+							backgroundColor: "#484848",
+							color: "white",
+							height: "40px",
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							gap: "5px",
+						}}
+					>
+						<Col>
+							Todos los derechos reservados · Copyright ©
+							{today.getFullYear().toString()} Anga Col ·
+							Desarrollado por el pinguino sabio de YAMMCODEV{""}
+						</Col>
+						<Col
+							style={{ filter: "grayscale(100%)", width: "30px" }}
+						>
+							<Image src={pingu} width={30}></Image>
+						</Col>
+					</Row>
+				</Row>
+			) : (
+				<>
+					<Divider style={{ width: "100%", visibility: "hidden" }} />
+					<UpFooter anchoPagina={anchoPagina} />
 
-						<Divider style={{ width: "100%" }} />
-						<DownFooter />
-						<Divider
-							style={{ width: "100%", visibility: "hidden" }}
-						/>
-					</>
-				)}
-			</Row>	
+					<Divider style={{ width: "100%" }} />
+					<DownFooter />
+					<Divider style={{ width: "100%", visibility: "hidden" }} />
+				</>
+			)}
+		</Row>
 	);
 };
 export default FooterComponent;
